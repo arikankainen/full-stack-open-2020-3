7cbbdc0c7fe8d772e6306ab41fc7c27114481db2
arikankainen <60932647+arikankainen@users.noreply.github.com>
2020-04-12 17:54:17 +0300

    3.9 puhelinluettelon backend step9

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 
-morgan.token('body', (req, res) => {
-  return JSON.stringify(req.body)
+morgan.token('body', (request, response) => {
+  return JSON.stringify(request.body)
 })
 
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 app.use(morgan(
   ':method :url :status :res[content-length] - :response-time ms :body'
 ))
