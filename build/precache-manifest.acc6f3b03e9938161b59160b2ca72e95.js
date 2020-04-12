5d9853b9dec86d6b8b612f2d6571b66cc501bc43
arikankainen <60932647+arikankainen@users.noreply.github.com>
2020-04-12 19:12:38 +0300

    uibuild

---
+++ build/precache-manifest.acc6f3b03e9938161b59160b2ca72e95.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34698b343d1c7368b173bda169b12008",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0994491725f0146a3389",
+    "url": "/static/css/main.78b86494.chunk.css"
+  },
+  {
+    "revision": "96fa2a80d0443e24a6d9",
+    "url": "/static/js/2.eb02a478.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.eb02a478.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0994491725f0146a3389",
+    "url": "/static/js/main.2356d11b.chunk.js"
+  },
+  {
+    "revision": "b9db865b90dc1d93d813",
+    "url": "/static/js/runtime-main.0f618fec.js"
+  }
+]);
